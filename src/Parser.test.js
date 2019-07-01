a5c3e8c091d4e0b6c920c05af1e8252499f26449
den <denis.vinokurov@gmail.com>
2019-07-02 02:06:50 +0300

    Rename tests-html to tests-data

--- src/Parser.test.js
+++ src/Parser.test.js
@@ -4,8 +4,8 @@ import Parser from "./Parser";
 describe("Parser", () => {
 
     test("parseTournamentsPageHtml parse tournaments-page.html to tournaments-page.json", () => {
-        const tournamentsPageHtml = fs.readFileSync("./tests-html/tournaments-page.html", "utf8");
-        const tournamentsPageHtmlResult = JSON.parse(fs.readFileSync("./tests-html/tournaments-page.json", "utf8"));
+        const tournamentsPageHtml = fs.readFileSync("./tests-data/tournaments-page.html", "utf8");
+        const tournamentsPageHtmlResult = JSON.parse(fs.readFileSync("./tests-data/tournaments-page.json", "utf8"));
 
         const parser = new Parser();
         const result = parser.parseTournamentsPageHtml(tournamentsPageHtml);
