5cf6ebc7789f070b9f4083ac33499c4a1aef5df6
den <denis.vinokurov@gmail.com>
2019-07-04 20:09:26 +0300

    Rename results file

--- src/Parser.test.js
+++ src/Parser.test.js
@@ -5,7 +5,7 @@ describe("Parser", () => {
 
     test("parseTournamentsPageHtml parse tournaments-page.html to tournaments-page.json", () => {
         const tournamentsPageHtml = fs.readFileSync("./tests-data/tournaments-page.html", "utf8");
-        const tournamentsPageHtmlResult = JSON.parse(fs.readFileSync("./tests-data/tournaments-page.json", "utf8"));
+        const tournamentsPageHtmlResult = JSON.parse(fs.readFileSync("./tests-data/parseTournamentsPageHtml.json", "utf8"));
 
         const parser = new Parser();
         const result = parser.parseTournamentsPageHtml(tournamentsPageHtml);
